2cc9077feba409198e032b1f91620f9d21adf09c
Сергей Долгов <55320380+dolgofff@users.noreply.github.com>
2023-07-09 21:49:53 +0300

    Add files via upload

---
+++ Task2/MyEqualityComparer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracTask2
+{
+    public class MyEqualityComparer<T> : IEqualityComparer<T>
+    {
+        //Default - автоматически сгенерированный компаратор для <T>
+        public bool Equals(T? x, T? y)
+        {
+            return EqualityComparer<T>.Default.Equals(x, y);
+        }
+
+        public int GetHashCode(T? obj)
+        {
+            return EqualityComparer<T>.Default.GetHashCode(obj);
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace PracTask2
+{
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                int[] nabor = new int[] { 1, 2, 3 };
+                var combinations1 = IEnumExtensions.Combinations(nabor,new MyEqualityComparer<int>(), 2 );
+                foreach(var item in combinations1)
+                {
+                    Console.WriteLine($"[{String.Join(",",item)}]");
+                }
+                Console.WriteLine(Environment.NewLine);
+
+                var subset = IEnumExtensions.Subsets(nabor,new MyEqualityComparer<int>());
+                foreach(var item in subset)
+                {
+                    Console.WriteLine($"[{String.Join(",", item)}]");
+                }
+                Console.WriteLine(Environment.NewLine);
+
+                var permutation = IEnumExtensions.Permutations(nabor,new MyEqualityComparer<int>());
+                foreach( var item in permutation)
+                {
+                    Console.WriteLine($"[{String.Join(",", item)}]");
+                }
+
+
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine($"Исключение: {ex.Message}");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Исключение: {ex.Message}");
+            }
+
+            /*  catch(ArgumentOutOfRangeException ex)
+              {
+                  Console.WriteLine($"Исключение: {ex.Message}");
+              }*/
+        }
+    }
+
+}
+
